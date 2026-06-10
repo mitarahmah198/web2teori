16ce9b0241213ed7d167af6eae62b3208335ee88
mita <mitarahmah198@gmail.com>
2026-06-11 02:58:05 +0800

    Add authentication and authorization with admin role

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,29 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-use App\Http\Controllers\BookController;
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/books', [BookController::class, 'index']);
-Route::post('/books', [BookController::class, 'store']);
-Route::put('/books/{id}', [BookController::class, 'update']);
-Route::delete('/books/{id}', [BookController::class, 'destroy']);
+Route::middleware('auth')->group(function () {
+
+    Route::get('/books', [BookController::class,'index']);
+    Route::post('/books', [BookController::class,'store']);
+    Route::put('/books/{id}', [BookController::class,'update']);
+    Route::delete('/books/{id}', [BookController::class,'destroy'])
+    ->middleware(['auth', 'admin']);
+});
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
